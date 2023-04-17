7523ae2825e7e28133c9dd556b05d1f3ad896341
M. A. Startsev <m.a.startsev@gmail.com>
2023-04-18 02:12:00 +0300

    task compled

--- src/components/App/exports.js
+++ src/components/App/exports.js
@@ -6,4 +6,5 @@ export { default as ContactList } from '../ContactList/ContactList';
 export { default as ContactListItem } from '../ContactListItem/ContactListItem';
 export { default as Filter } from '../Filter/Filter';
 
+export { default as contactsTest } from '../../contactsTest.json';
 export { Container, Title, SubTitle } from './AppElements';
